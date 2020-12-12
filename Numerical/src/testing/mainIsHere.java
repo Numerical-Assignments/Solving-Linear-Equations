c69e6251407368cb34313ac201c7a3f252173385
Ahmed Ashraf <Ahmed Ashraf@DESKTOP-24MGP8E>
2020-12-12 13:49:37 +0200

    add commit ahmed

--- Numerical/src/testing/mainIsHere.java
+++ Numerical/src/testing/mainIsHere.java
@@ -1,9 +1,25 @@
 package testing;
 
+import methods.GaussSeidil;
+import methods.JacobiIteration;
+
 public class mainIsHere{
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//System.out.println("Hello World!");
+		double [][]matrix = {{1,2},{6,5}};
+		double []b = {3,4};
+		GaussSeidil gs = new GaussSeidil();
+		double[] s = gs.gaussSeidil(matrix, b,1e-15, 7);
+		for (int i =0 ; i< s.length;i++) {
+			System.out.println(s[i]);
+		}
+		JacobiIteration j = new JacobiIteration();
+		double[] ss = j.jacobi(matrix, b,1e-15, 7);
+		for (int i =0 ; i< ss.length;i++) {
+			System.out.println(ss[i]);
+		}
+
 	}
 
 }
