75550555b51acf18ec09c2976d5045b572580140
Ahmed Tarek <ahmedkatoos10@gmail.com>
2020-12-14 14:31:27 +0200

    3 Forms of LU Decomposition with precision

--- Numerical/src/methods/LUDecomposition.java
+++ Numerical/src/methods/LUDecomposition.java
@@ -1,10 +1,12 @@
 package methods;
 
 public class LUDecomposition {
-  static void LUDoolittle(double[][] mat, double [] b, int n) {
+	    static String LUDoolittle(double[][] mat, double [] b, int n, int p) {
 	    	
 	        double[][] lower = new double[n][n];
 	        double[][] upper = new double[n][n];
+	        
+	   	 	StringBuilder steps = new StringBuilder();
 	 
 	        // Decomposing matrix into Upper and Lower
 	        // triangular matrix
@@ -16,10 +18,13 @@ public class LUDecomposition {
 	                // Summation of L(i, j) * U(j, k)
 	               double sum = 0;
 	                for (int j = 0; j < i; j++)
-	                    sum += (lower[i][j] * upper[j][k]);
+	                    sum += setpercision((lower[i][j] * upper[j][k]),p);
+	                	sum = setpercision(sum, p);
 	 
 	                // Evaluating U(i, k)
-	                upper[i][k] = mat[i][k] - sum;
+	                upper[i][k] = setpercision(mat[i][k],p) - sum;
+	                upper[i][k] = setpercision(upper[i][k], p);
+	                
 	            }
 	 
 	            // Lower Triangular
@@ -32,37 +37,42 @@ public class LUDecomposition {
 	                    // Summation of L(k, j) * U(j, i)
 	                    double sum = 0;
 	                    for (int j = 0; j < i; j++)
-	                        sum += (lower[k][j] * upper[j][i]);
-	 
+	                        sum += setpercision((lower[k][j] * upper[j][i]), p);
+	                		sum = setpercision(sum, p);
+
 	                    // Evaluating L(k, i)
-	                    lower[k][i]
-	                        = (mat[k][i] - sum) / upper[i][i];
+	                    lower[k][i] = (mat[k][i] - sum) / upper[i][i];
+	                    lower[k][i] = setpercision(lower[k][i], p);
 	                }
 	            }
 	        }
-	        
-	        System.out.println("Lower Triangular");
+	        steps.append("Lower Triangular" +"\n");
+	        //System.out.println("Lower Triangular");
 	       
 	        // Displaying the result :
 	        // Lower
 	        
 	        for (int i = 0; i < n; i++) {
 	            for (int j = 0; j < n; j++) {
-	                System.out.print(lower[i][j] + "\t");
+	    	        steps.append(lower[i][j] + "\t");
+	                //System.out.print(lower[i][j] + "\t");
 	            }
-	            System.out.print("\n");
+		        steps.append("\n");
+	            //System.out.print("\n");
 	        }
-	        
-	        System.out.println("Upper Triangular");	        
+	        steps.append("Upper Triangular" + "\n");
+	        //System.out.println("Upper Triangular");	        
 	        
 	        // Displaying the result :
             // Upper
 
 	        for (int i = 0; i < n; i++) {
 	            for (int j = 0; j < n; j++) {
-	                System.out.print(upper[i][j] + "\t");
+	    	        steps.append(upper[i][j] + "\t");
+	                //System.out.print(upper[i][j] + "\t");
 	            }
-	            System.out.print("\n");
+		        steps.append("\n");
+	            //System.out.print("\n");
 	        }
 	        
 	        //finding the solution 
@@ -70,25 +80,29 @@ public class LUDecomposition {
 		    //LUX = B
 	        //LY = B
 	        //UX = Y
-	        double ff[] = forward (lower, b);
-		    
-		    double bb[]= backward (upper, ff);
-		    System.out.println("Solution");	 
+	        double ff[] = forward (lower, b, p);
+		    double bb[]= backward (upper, ff, p);
+	        
+		    steps.append("Solution" + "\n");
+		    //System.out.println("Solution");	 
 		       
 		    for (double element: bb) {
-		    	System.out.println(element);
+			    steps.append(element + "\n");
+		    	//System.out.println(element);
 		    }
-		    System.out.println();
-	    }
-  
+	        steps.append("\n");
+		    //System.out.println();
+		    return steps.toString();
+	    }  
 	// Method  (Crout LU Method
 	
-	public static void LUCrout(double mat [][], double [] b, int n) {
+	public static String LUCrout(double mat [][], double [] b, int n, int p) {
 		int i, j, k;
 		double sum = 0;
-        	double[][] lower = new double[n][n];
-        	double[][] upper = new double[n][n];
-        
+        double[][] lower = new double[n][n];
+        double[][] upper = new double[n][n];
+   	 	StringBuilder steps = new StringBuilder();
+
 		for (i = 0; i < n; i++) {
 			upper[i][i] = 1;
 		}
@@ -97,63 +111,82 @@ public class LUDecomposition {
 			for (i = j; i < n; i++) {
 				sum = 0;
 				for (k = 0; k < j; k++) {
-					sum = sum + lower[i][k] * upper[k][j];	
+					sum = sum + setpercision((lower[i][k] * upper[k][j]), p);
+					sum = setpercision(sum,p);
 				}
-				lower[i][j] = mat[i][j] - sum;
+				lower[i][j] = setpercision(mat[i][j],p) - sum;
+				lower[i][j] = setpercision(lower[i][j],p);
 			}
 
 			for (i = j; i < n; i++) {
 				sum = 0;
 				for(k = 0; k < j; k++) {
-					sum = sum + lower[j][k] * upper[k][i];
+					sum = sum + setpercision((lower[j][k] * upper[k][i]),p);
+					sum = setpercision(sum,p);
+
 				}
 				if (lower[j][j] == 0) {
-					System.out.println("det(L) close to 0!\n Can't divide by 0...\n");
-					return ;
+					//System.out.println("det(L) close to 0!\n Can't divide by 0...\n");
+					
+					steps.append("det(L) close to 0!\n Can't divide by 0...\n");
+					return steps.toString();
 				}
-				upper[j][i] = (mat[j][i] - sum) / lower[j][j];
+				upper[j][i] = (setpercision(mat[j][i],p) - sum) / setpercision(lower[j][j],p);
+				upper[j][i] = setpercision(upper[j][i], p);
 			}
 		}
 		
-        System.out.println("Lower Triangular");
-	       
+        steps.append("Lower Triangular" +"\n");
+        //System.out.println("Lower Triangular");
+       
         // Displaying the result :
         // Lower
         
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                System.out.print(lower[i][j] + "\t");
+        for ( i = 0; i < n; i++) {
+            for ( j = 0; j < n; j++) {
+    	        steps.append(lower[i][j] + "\t");
+                //System.out.print(lower[i][j] + "\t");
             }
-            System.out.print("\n");
+	        steps.append("\n");
+            //System.out.print("\n");
         }
-        
-        System.out.println("Upper Triangular");	        
+        steps.append("Upper Triangular" + "\n");
+        //System.out.println("Upper Triangular");	        
         
         // Displaying the result :
         // Upper
 
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                System.out.print(upper[i][j] + "\t");
+        for ( i = 0; i < n; i++) {
+            for ( j = 0; j < n; j++) {
+    	        steps.append(upper[i][j] + "\t");
+                //System.out.print(upper[i][j] + "\t");
             }
-            System.out.print("\n");
-        }
-        
-        double ff[] = forward (lower, b);
-	    double bb[]= backward (upper, ff);
-	    System.out.println("Solution");	 
+	        steps.append("\n");
+            //System.out.print("\n");
+        }        
+        double ff[] = forward (lower, b, p);
+	    double bb[]= backward (upper, ff, p);
+	    steps.append("Solution" + "\n");
+	    //System.out.println("Solution");	 
 	       
 	    for (double element: bb) {
-	    	System.out.println(element);
+		    steps.append(element + "\n");
+	    	//System.out.println(element);
 	    }
-	    System.out.println();
+        steps.append("\n");
+	    //System.out.println();
+	    return steps.toString();
         
 	}
+
 	
   // Method 3 ( Cholesky LU Decomposition )
   
-  static void LUCholesky(double[][] matrix, double [] b, int n) {
-    	if (isSymmetric(matrix)) {
+	static String LUCholesky(double[][] matrix, double [] b, int n, int p) {
+    	
+   	 	StringBuilder steps = new StringBuilder();
+
+		if (isSymmetric(matrix)) {
     		double[][] lower = new double[n][n];
         	double[][] lowerTranspose = new double[n][n];
         	// Decomposing a matrix
@@ -165,66 +198,82 @@ public class LUDecomposition {
         			if (j == i) {
         				for (int k = 0; k < j; k++)
         					sum += (int)Math.pow(lower[j][k], 2);
+        					sum = setpercision(sum, p);
         					lower[j][j] = (int)Math.sqrt(matrix[j][j] - sum);
+        					lower[j][j] = setpercision(lower[j][j],p);
         			}
         			else {
         				// Evaluating L(i, j)
         				// using L(j, j)
         				for (int k = 0; k < j; k++)
         					sum += (lower[i][k] * lower[j][k]);
+    						sum = setpercision(sum, p);
         					lower[i][j] = (matrix[i][j] - sum) / lower[j][j];
+        					lower[i][j] = setpercision(lower[i][j],p);
+
         			}
         		}
         	}
         	transpose(lower,lowerTranspose);
         	
-            System.out.println("Lower Triangular");
-    	       
-        	// Displaying Lower
-            
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(lower[i][j] + "\t");
-                }
-                System.out.print("\n");
-            }
-            
-            System.out.println("Lower Triangular Transpose");	        
-            
+            steps.append("Lower Triangular" +"\n");
+	        //System.out.println("Lower Triangular");
+	       
+	        // Displaying the result :
+	        // Lower
+	        
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < n; j++) {
+	    	        steps.append(lower[i][j] + "\t");
+	                //System.out.print(lower[i][j] + "\t");
+	            }
+		        steps.append("\n");
+	            //System.out.print("\n");
+	        }
+	        steps.append("Lower Triangular Transpose" + "\n");
+	        //System.out.println("Lower Triangular Transpose");	        
+	        
     	    // Transpose of
     	    // Lower Triangular
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    System.out.print(lowerTranspose[i][j] + "\t");
-                }
-                System.out.print("\n");
-            }
-            
+	        for (int i = 0; i < n; i++) {
+	            for (int j = 0; j < n; j++) {
+	    	        steps.append(lowerTranspose[i][j] + "\t");
+	                //System.out.print(lowerTranspose[i][j] + "\t");
+	            }
+		        steps.append("\n");
+	            //System.out.print("\n");
+	        }
+	        //finding the solution 
     	    //LUX = B
             //LY = B
             //UX = Y
             
-            double ff[] = forward (lower, b);
-    	    double bb[]= backward (lowerTranspose, ff);
-    	    System.out.println("Solution");	 
-    	       
-    	    for (double element: bb) {
-    	    	System.out.println(element);
-    	    }
-    	    System.out.println();
+            double ff[] = forward (lower, b, p);
+    	    double bb[]= backward (lowerTranspose, ff, p);
+		    steps.append("Solution" + "\n");
+		    //System.out.println("Solution");	 
+		       
+		    for (double element: bb) {
+			    steps.append(element + "\n");
+		    	//System.out.println(element);
+		    }
+	        steps.append("\n");
+		    //System.out.println();
+		    return steps.toString();
     	}
     	
     	else {
     	    System.out.println("Not Symmetric Matrix :)");
-
+    	    steps.append("Not Symmetric Matrix :)"+"\n");
+		    return steps.toString();
+    	    
     	}
 		
-    }
+    }  
+  	// To Check matrix is symmetric or not for cholesky decomposition
   
-  // To Check matrix is symmetric or not for cholesky decomposition
-  
-  static boolean isSymmetric(double mat[][]) 
+  	static boolean isSymmetric(double mat[][]) 
 	{ 
 	    for (int i = 0; i < mat.length; i++) 
 	        for (int j = 0; j < mat[0].length; j++) 
@@ -232,10 +281,17 @@ public class LUDecomposition {
 	                return false; 
 	    return true; 
 	}
+	
+	// precision
   
-    // doing backward substitution 
-
-	public static double[] backward( double[][] mat,double b[]) {
+	public static double setpercision(double Number, int percision) {
+		Double truncatedDouble = BigDecimal.valueOf(Number).setScale(percision, RoundingMode.HALF_UP).doubleValue();
+		return truncatedDouble;
+	}
+	
+	// backward substitution
+	
+	public static double[] backward( double[][] mat,double b[], int p) {
 		
 		int nrow =mat.length;
 		int ncol = mat[0].length;  
@@ -245,17 +301,19 @@ public class LUDecomposition {
 		for(int r=nrow-1; r>=0; r--) {
 			double val =0;
 			for(int c=ncol-1;c>r;c--) {
-				val =val +  sol[c] * mat[r][c];  
+				val = val + setpercision((sol[c] * mat[r][c]), p);  
+				val = setpercision(val, p);
 			}
-			val = b[r] - val;
-			sol[r] = val/ mat[r][r]; 
+			val = setpercision(b[r],p) - val;
+			sol[r] = val/ setpercision(mat[r][r],p);
+			sol[r] = setpercision(sol[r], p);
 	      	}     
 		return sol; 
 	}
 	
-  // doing forward substitution 
-  
-	public static double[] forward(double[][] mat,double b[]) {
+	// forward substitution
+	
+	public static double[] forward(double[][] mat,double b[],int p) {
 	  
 		int nrow =mat.length;  
 		
@@ -264,10 +322,12 @@ public class LUDecomposition {
 		for (int r=0;r<nrow; r++) {
 			double val =0;   
 			for (int c=0;c<r; c++) {
-				val =val +  sol[c] *mat[r][c];     
+				val = val + setpercision((sol[c] * mat[r][c]), p);  
+				val = setpercision(val, p);
 			}    
-			val = b[r] - val;
-			sol[r] = val/mat[r][r];
+			val = setpercision(b[r],p) - val;
+			sol[r] = val/ setpercision(mat[r][r],p);
+			sol[r] = setpercision(sol[r], p);
 		}        
 		return sol;
 	}
@@ -285,12 +345,14 @@ public class LUDecomposition {
 	
 public static void main(String[] args) {
         int n = 3;
+	int precision;
 	double mat[][] = { { 4, 12, -16 }, { 12, 37, -43 }, { -16, -43, 98 } };
         double b[] = {6, 7, 15};
 
-	LUDoolittle(mat, b, n);
-        LUCholesky(mat, b, n);
-	LUCrout(mat, b, n);
+	//LUDoolittle(mat, b, n);
+	System.out.println(LUDoolittle(mat, b, n, precision));
+        System.out.println(LUCholesky(mat, b, n, precision));
+        System.out.println(LUCrout(mat,m, b, precision));
 
     }
   
